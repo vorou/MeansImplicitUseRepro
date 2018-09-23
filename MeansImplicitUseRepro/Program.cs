5290bac2439d4feadf5f5e96edd00e4bc4c797f2
vorou <voropaev.viktor@gmail.com>
2018-09-23 12:43:39 +0500

    init

---
+++ MeansImplicitUseRepro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+using SimpleContainer.Infection;
+
+namespace MeansImplicitUseRepro
+{
+	internal static class Program
+	{
+		public static void Main()
+		{
+			var a = new Bombaclat(null);
+			a.Do();
+		}
+	}
+
+	public class Bombaclat
+	{
+		private IArgumentProvider notUsedAtAll;
+
+		[Inject]
+		private IArgumentProvider assignedImplicitlyButNotUsed;
+
+		private IArgumentProvider assignedExplicitlyButNotUsed;
+		
+		[Inject]
+		private IArgumentProvider assignedAndUsed;
+		
+		public Bombaclat(IArgumentProvider assignedExplicitlyButNotUsed)
+		{
+			this.assignedExplicitlyButNotUsed = assignedExplicitlyButNotUsed;
+		} 
+		
+		public void Do()
+		{
+			Console.Out.WriteLine(assignedAndUsed);
+		}
+	}
+}
